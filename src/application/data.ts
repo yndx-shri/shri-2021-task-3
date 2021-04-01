be6e02169b1690fdfa12af6f16f3f2bcc121acd5
Egor18032019 <goro4@mail.ru>
2021-04-01 17:15:43 +0500

    поправил пути immer

--- src/application/data.ts
+++ src/application/data.ts
@@ -1,4 +1,4 @@
-import produce, { Draft } from 'immer';
+import { produce, Draft } from "immer"
 
 import { Action } from './actions';
 import { descriptors, DRAFT_STATE, errors, INTERVAL, State } from './types';
